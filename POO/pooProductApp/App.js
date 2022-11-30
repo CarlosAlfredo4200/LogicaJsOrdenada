b449cb5a8e5c09d58f4106a38dee712b53e9541b
Carlos <alfredomontoyagit@gmail.com>
2022-11-29 19:56:30 -0500

    Poo eleminar div

--- POO/pooProductApp/App.js
+++ POO/pooProductApp/App.js
@@ -1,49 +1,63 @@
- class Product{
-    
-    constructor(name, price, year){
-        this.name = name;
-        this.price = price;
-        this.year = year;
-    }
- }
+class Product {
+  constructor(name, price, year) {
+    this.name = name;
+    this.price = price;
+    this.year = year;
+  }
+}
 
-
- class UI {
-    addProduct(product){
-      const product_list=  document.getElementById("product-list");
-      const element = document.createElement('div');
-      element.innerHTML = `
+class UI {
+  addProduct(product) {
+    const product_list = document.getElementById("product-list");
+    const element = document.createElement("div");
+    element.innerHTML = `
       <div class="card_info">
       <div class="card_body">
-          <strong>Product</strong>:${product.name}
-          <strong>Price</strong>:${product.price}
-          <strong>Year</strong>:${product.year}
+          <strong>Product</strong>: ${product.name}
+          <strong>Price</strong>: ${product.price}
+          <strong>Year</strong>: ${product.year}
+          <a href="#" name="delete">Delete</a>
       </div>
   </div>  
 
       `;
-      product_list.appendChild(element);
+    product_list.appendChild(element);
     
-    }
+  }
 
-    deleteProduct(){
-        
-    }
+  resertForm(){
+    document.getElementById("product-form").reset();
+  }
 
-    showMessage(){
+  deleteProduct(element) {
+   if(element.name =="delete"){
+      element.parentElement.parentElement.parentElement.remove();
+   }
+  }
+    
+  showMessage() {}
+}
 
-    }
- }
+//DOM events
 
- //DOM events
+document
+  .getElementById("product-form")
+  .addEventListener("submit", function (e) {
+    const namePro = document.getElementById("name").value;
+    const pricePro = document.getElementById("price").value;
+    const yearPro = document.getElementById("year").value;
 
- document.getElementById("product-form").addEventListener("submit", function(e) {
-   const namePro = document.getElementById("name").value;
-   const pricePro = document.getElementById("price").value;
-   const yearPro = document.getElementById("year").value;
-   
-   const product = new Product(namePro, pricePro, yearPro);
-   const ui = new UI();
-   ui.addProduct(product);
-   e.preventDefault();
- })
+    const product = new Product(namePro, pricePro, yearPro);
+    const ui = new UI();
+    ui.addProduct(product);
+
+    ui.resertForm();
+    e.preventDefault();
+  });
+
+
+document.getElementById("product-list").addEventListener("click", function (e) {
+  const ui = new UI();
+  ui.deleteProduct(e.target);
+
+})
