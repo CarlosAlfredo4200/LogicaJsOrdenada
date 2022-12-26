0ddf9436a4f8f5ea062f308e5233f006593dc763
Carlos <alfredomontoyagit@gmail.com>
2022-12-26 13:02:09 -0500

    Ordenamiento correción

--- ejerArray/ordenarArray.js
+++ ejerArray/ordenarArray.js
@@ -1,42 +1,64 @@
 // Ordenando con map
 
 // el array a ordenar
-var list = ['Delta', 'alpha', 'CHARLIE', 'bravo'];
+var list = ["Delta", "alpha", "CHARLIE", "bravo"];
 
 // array temporal contiene objetos con posición y valor de ordenamiento
-var mapped = list.map(function(el, i) {
+var mapped = list.map(function (el, i) {
   return { index: i, value: el.toLowerCase() };
-})
+});
 
-// ordenando el array mapeado que contiene los valores reducidos
-mapped.sort(function(a, b) {
-  if (a.value > b.value) {
-    return 1;
+//console.log(mapped);
+
+//------------- ordenando el array mapeado que contiene los valores reducidos-------
+
+//------------- Algoritmo burbuja -------------------//
+
+const arrayNumeros = [9, 3, 6, 4, 7, -3];
+
+function ordenamientoLogico(arr) {
+  for (let k = 0; k < arr.length - 1; k++) {
+    for (let i = 0; i < arr.length - k - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+      }
+    }
   }
-  if (a.value < b.value) {
+  console.log(`\nFunción de ordenamiento :${arr}`);
+}
+ordenamientoLogico(arrayNumeros);
+
+console.log(`\nOrdenamiento con sort ${arrayNumeros.sort()}`);
+
+ 
+const mayorAmenor = arrayNumeros.sort( function (b, a) { 
+  if(a==b){
+    return 0;
+  }
+  if(a < b){
     return -1;
   }
-  return 0;
-});
-
-// contenedor para el orden resultante
-var result = mapped.map(function(el){
-  return list[el.index];
-});
-
+  return 1});
+ 
+console.log("\nOrdenamiento if :" +mayorAmenor);
 // -----------------------------------------------------
 
 // Un array de elementos numéricos, usando una función de comparación:
 
-var arr = [ 40, 1, 5, 200 ];
-function comparar ( a, b ){ return a - b; }
-arr.sort( comparar );  // [ 1, 5, 40, 200 ]
+var arr = [40, 1, 5, 200];
+
+function comparar(a, b) {
+  return a - b;
+}
+arr.sort(comparar); // [ 1, 5, 40, 200 ]
 
 // Lo mismo pero usando una función anónima normal:
 
-var arr = [ 40, 1, 5, 200 ];
-arr.sort(function(a,b){return a - b;});  // [ 1, 5, 40, 200 ]
+var arr = [40, 1, 5, 200];
+arr.sort(function (a, b) {
+  return a - b;
+}); // [ 1, 5, 40, 200 ]
 
 // Lo mismo escrito más compacto mediante una función flecha:
-var arr = [ 40, 1, 5, 200 ];
-arr.sort((a,b)=>a-b);  // [ 1, 5, 40, 200 ]
+var arr = [40, 1, 5, 200];
+arr.sort((a, b) => a - b); // [ 1, 5, 40, 200 ]
